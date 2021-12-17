3824ed11597045fece3e87efc7f9f185a80f3a27
AlexMartPr <alexmartpr@gmail.com>
2021-12-17 23:36:04 +0300

    Scripted auto-commit on change (2021-12-17 23:36:04) by gitwatch.sh

--- src/main/java/AnonymizerZooApp.java
+++ src/main/java/AnonymizerZooApp.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class AnonymizerZooApp {
     public static void main(String[] args) throws IOException {
@@ -16,6 +18,9 @@ public class AnonymizerZooApp {
         try {
             port = Integer.parseInt(args[0]);
             ZookeeperWatcher watcher = new ZookeeperWatcher(storageActor, port);
+            final Http http = Http.get(system);
+            final ActorMaterializer mat = ActorMaterializer.create(system);
+            final Flow<HttpRequest, HttpResponse, NotUsed> flow;
         } catch (NumberFormatException e) {
             e.printStackTrace();
             System.exit(-1);
