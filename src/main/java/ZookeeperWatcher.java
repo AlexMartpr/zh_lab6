4436530656e452f7eb4c29600923c339c9debf7c
AlexMartPr <alexmartpr@gmail.com>
2021-12-17 21:46:04 +0300

    Scripted auto-commit on change (2021-12-17 21:46:04) by gitwatch.sh

--- src/main/java/ZookeeperWatcher.java
+++ src/main/java/ZookeeperWatcher.java
@@ -1,7 +1,15 @@
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
+import akka.actor.ActorRef;
+
 public class ZookeeperWatcher implements Watcher {
+    private final ActorRef storageActor;
+    
+    public ZookeeperWatcher(ActorRef sActor) {
+        this.storageActor = sActor;
+    }
+
 
     @Override
     public void process(WatchedEvent event) {
