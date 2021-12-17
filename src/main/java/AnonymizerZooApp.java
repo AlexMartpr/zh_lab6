3bae46995ba199fd5c1ccb90ccacb4c9f5bbc935
AlexMartPr <alexmartpr@gmail.com>
2021-12-17 21:25:40 +0300

    Scripted auto-commit on change (2021-12-17 21:25:40) by gitwatch.sh

--- src/main/java/AnonymizerZooApp.java
+++ src/main/java/AnonymizerZooApp.java
@@ -1,5 +1,10 @@
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
 public class AnonymizerZooApp {
     public static void main(String[] args) {
-        
+        final ActorSystem system = ActorSystem.create("routes");
+        ActorRef storageActor = system.actorOf(Props.create(ConfigStorage.class));
     }
 }
