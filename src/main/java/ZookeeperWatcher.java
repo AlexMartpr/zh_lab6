207c68d0424594d015472f038158102186c01311
AlexMartPr <alexmartpr@gmail.com>
2021-12-17 21:49:01 +0300

    Scripted auto-commit on change (2021-12-17 21:49:01) by gitwatch.sh

--- src/main/java/ZookeeperWatcher.java
+++ src/main/java/ZookeeperWatcher.java
@@ -1,15 +1,19 @@
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 import akka.actor.ActorRef;
 
 public class ZookeeperWatcher implements Watcher {
     private final String CONNECET = "127.0.0.1:2181";
     private final Integer SESSION_TIMEOUT = 3000;
+
     private final ActorRef storageActor;
+    private final ZooKeeper zoo;
     
     public ZookeeperWatcher(ActorRef sActor) {
         this.storageActor = sActor;
+        this.zoo = new ZooKeeper(CONNECET, SESSION_TIMEOUT, watcher);
     }
 
 
