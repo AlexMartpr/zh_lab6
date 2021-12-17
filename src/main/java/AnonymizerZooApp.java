cfc9265c807b64af013e0b7fce342fbbf8a4e089
AlexMartPr <alexmartpr@gmail.com>
2021-12-17 21:36:36 +0300

    Scripted auto-commit on change (2021-12-17 21:36:36) by gitwatch.sh

--- src/main/java/AnonymizerZooApp.java
+++ src/main/java/AnonymizerZooApp.java
@@ -6,5 +6,17 @@ public class AnonymizerZooApp {
     public static void main(String[] args) {
         final ActorSystem system = ActorSystem.create("routes");
         ActorRef storageActor = system.actorOf(Props.create(ConfigStorage.class));
+        if (args.length != 1) {
+            System.out.println("Missed argument for Port. Returning...");
+            System.exit(-1);
+        }
+        int port;
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        ZookeeperWatcher watcher = new ZookeeperWatcher();
     }
 }
