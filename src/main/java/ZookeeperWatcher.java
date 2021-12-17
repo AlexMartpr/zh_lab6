b5046aeea7c943e2d2475bfe154aede23a8cd02f
AlexMartPr <alexmartpr@gmail.com>
2021-12-17 22:56:20 +0300

    Scripted auto-commit on change (2021-12-17 22:56:20) by gitwatch.sh

--- src/main/java/ZookeeperWatcher.java
+++ src/main/java/ZookeeperWatcher.java
@@ -1,13 +1,18 @@
 import java.io.IOException;
+import java.util.List;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
 import akka.actor.ActorRef;
 
 public class ZookeeperWatcher implements Watcher {
     private final String CONNECET = "127.0.0.1:2181";
+    private final String PATH = "/servers/s";
     private final Integer SESSION_TIMEOUT = 3000;
 
     private final ActorRef configStorageActor;
@@ -22,14 +27,27 @@ public class ZookeeperWatcher implements Watcher {
     }
 
     private void CreateZooServers() {
-
+        String adress = "127.0.0.1:" + this.currentPort;
+        try {
+            this.zoo.create(PATH,
+                            adress.getBytes(),
+                            ZooDefs.Ids.OPEN_ACL_UNSAFE, 
+                            CreateMode.EPHEMERAL_SEQUENTIAL);
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 
-
     @Override
-    public void process(WatchedEvent event) {
-        
-        
+    public void process(WatchedEvent event) {        
+        List<String> servers;
+        try {
+            servers = this.zoo.getChildren(PATH.substring(0, PATH.length() - 1), this);
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
     
 }
