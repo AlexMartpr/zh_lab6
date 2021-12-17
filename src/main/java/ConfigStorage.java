e14672f0db581dc2b61bd6c8cc86aee6341990f2
AlexMartPr <alexmartpr@gmail.com>
2021-12-17 23:23:53 +0300

    Scripted auto-commit on change (2021-12-17 23:23:53) by gitwatch.sh

--- src/main/java/ConfigStorage.java
+++ src/main/java/ConfigStorage.java
@@ -1,13 +1,18 @@
 import java.util.ArrayList;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class ConfigStorage extends AbstractActor {
     private ArrayList<String> storageServers;
 
     @Override
     public Receive createReceive() {
-        return Re;
+        return ReceiveBuilder.create()
+        .match(MessageServer.class, msg -> this.storageServers=msg.getUrls())
+        .match(, externalPredicate, apply)
     }
     
+
+
 }
