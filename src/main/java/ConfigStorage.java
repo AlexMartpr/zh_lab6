cfa74254651177f212efd8a984032aea6d97e663
AlexMartPr <alexmartpr@gmail.com>
2021-12-17 23:18:05 +0300

    Scripted auto-commit on change (2021-12-17 23:18:05) by gitwatch.sh

--- src/main/java/ConfigStorage.java
+++ src/main/java/ConfigStorage.java
@@ -1,3 +1,13 @@
-public class ConfigStorage {
+import java.util.ArrayList;
+
+import akka.actor.AbstractActor;
+
+public class ConfigStorage extends AbstractActor {
+    private ArrayList<String> storageServers;
+
+    @Override
+    public Receive createReceive() {
+        return Re;
+    }
     
 }
