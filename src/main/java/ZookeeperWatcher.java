76c2a87ee52af7891c195a0cbeb025cc3fb140f5
AlexMartPr <alexmartpr@gmail.com>
2021-12-18 02:32:28 +0300

    Scripted auto-commit on change (2021-12-18 02:32:28) by gitwatch.sh

--- src/main/java/ZookeeperWatcher.java
+++ src/main/java/ZookeeperWatcher.java
@@ -16,7 +16,7 @@ public class ZookeeperWatcher implements Watcher {
     private final String CONNECET = "127.0.0.1:2181";
     private final String HOST = "127.0.0.1";
     private final String PATH = "/servers/s";
-    private final Integer SESSION_TIMEOUT = 3000;
+    private final Integer SESSION_TIMEOUT = 5000;
 
     private final ActorRef configStorageActor;
     private final ZooKeeper zoo;
