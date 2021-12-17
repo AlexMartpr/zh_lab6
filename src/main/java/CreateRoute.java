fe6809ef9238700399d252b383e5c06452b84afc
AlexMartPr <alexmartpr@gmail.com>
2021-12-18 00:16:10 +0300

    Scripted auto-commit on change (2021-12-18 00:16:10) by gitwatch.sh

--- src/main/java/CreateRoute.java
+++ src/main/java/CreateRoute.java
@@ -1,8 +1,14 @@
+import java.time.Duration;
+import java.util.regex.Pattern;
+
 import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 
 public class CreateRoute {
+    private final int TIMEOUT = 5;
+
     private final Http http;
     private final ActorRef confActor;
 
@@ -10,11 +16,26 @@ public class CreateRoute {
     public CreateRoute(Http http, ActorRef conf) {
         this.http = http;
         this.confActor = conf;
-        return this.createRoute();
     }
 
 
     private Route createRoute() {
+        return route(get(() -> 
+            parameter(URL_STRING, url ->
+                parameter(COUNT_STRING, count -> {
+                    if (Integer.parseInt(count) == 0) {
+                        return completeWithFuture(this.http.singleRequest(HttpRequest.create(utl)));
+                    } else {
+                        return completeWithFuture(
+                            Patterns.ask(this.confActor
+                                        ,new EmptyMessage(),
+                                        Duration.ofSeconds(TIMEOUT))
+                                    .thenApply(serverUrl -> (String)serverUrl)    
+                        );
+                    }   
+                }
+            )
+        ));
     }
 
 }
