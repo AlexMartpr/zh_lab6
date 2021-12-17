bf62585973204a614fe4588e593a24c0c4a2f762
AlexMartPr <alexmartpr@gmail.com>
2021-12-17 23:49:11 +0300

    Scripted auto-commit on change (2021-12-17 23:49:11) by gitwatch.sh

--- src/main/java/AnonymizerZooApp.java
+++ src/main/java/AnonymizerZooApp.java
@@ -1,12 +1,19 @@
 import java.io.IOException;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 public class AnonymizerZooApp {
+    private static final String URL_STRING = "url";
+    private static final String COUNT_STRING = "count";
     public static void main(String[] args) throws IOException {
         final ActorSystem system = ActorSystem.create("routes");
         ActorRef storageActor = system.actorOf(Props.create(ConfigStorage.class));
@@ -27,4 +34,13 @@ public class AnonymizerZooApp {
         }
 
     }
+
+    public Route createRouter() {
+        return route(get(() -> 
+            parameter(URL_STRING, url ->
+                parameter(COUNT_STRING, count)
+            )
+        ));
+    }
+
 }
