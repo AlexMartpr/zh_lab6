c0e538513c8b09c45971c0281af9bc61a3492e43
AlexMartPr <alexmartpr@gmail.com>
2021-12-17 23:13:54 +0300

    Scripted auto-commit on change (2021-12-17 23:13:54) by gitwatch.sh

---
+++ src/main/java/MessageServer.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class MessageServer {
+    private final ArrayList<String> urls;
+
+    public MessageServer(ArrayList<String> urls) {
+        this.urls = urls;
+    }
+
+    public ArrayList<String> getUrls() {
+        return this.urls;
+    }
+
+}
