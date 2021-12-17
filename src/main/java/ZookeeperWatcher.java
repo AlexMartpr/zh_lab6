e4fc65206aa982b98ca7aedac0b3aac7f6616626
AlexMartPr <alexmartpr@gmail.com>
2021-12-17 21:48:08 +0300

    Scripted auto-commit on change (2021-12-17 21:48:08) by gitwatch.sh

--- src/main/java/ZookeeperWatcher.java
+++ src/main/java/ZookeeperWatcher.java
@@ -4,6 +4,8 @@ import org.apache.zookeeper.Watcher;
 import akka.actor.ActorRef;
 
 public class ZookeeperWatcher implements Watcher {
+    private final String CONNECET = "127.0.0.1:2181";
+    private final Integer SESSION_TIMEOUT = 3000;
     private final ActorRef storageActor;
     
     public ZookeeperWatcher(ActorRef sActor) {
