878d58da23f15c7f0d9e79c5a3e9a6d4a14c3abd
AlexMartPr <alexmartpr@gmail.com>
2021-12-17 21:36:46 +0300

    Scripted auto-commit on change (2021-12-17 21:36:46) by gitwatch.sh

--- src/main/java/ZookeeperWatcher.java
+++ src/main/java/ZookeeperWatcher.java
@@ -1,5 +1,12 @@
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
 public class ZookeeperWatcher implements Watcher {
+
+    @Override
+    public void process(WatchedEvent event) {
+        
+        
+    }
     
 }
